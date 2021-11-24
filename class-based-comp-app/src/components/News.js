bec422bd432dd8eb1ddb653bcefae3db55e7471f
Durwankur Naik <ndurwankur14@gmail.com>
2021-11-24 18:40:41 +0530

    Finished adding the infinite scroll and everything works completely fine
    The app keeps on fetching articles once you hit the end of the page and serves it to you. It keeps on doing it until there are none articles left for fetch from the API.

--- class-based-comp-app/src/components/News.js
+++ class-based-comp-app/src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItems from './NewsItems'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export class News extends Component {
   static defaultProps = {
@@ -16,75 +17,69 @@ export class News extends Component {
     pageSize: PropTypes.number
   }
 
-  constructor() {
-    super();
+  capitalizeFirst = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
-      loading: false,
       page: 1,
       disableNext: false,
       totalResults: 0
     }
+    document.title = `${this.capitalizeFirst(this.props.category)} -  Florange News`;
   }
 
-  async componentDidMount() {
-    this.setState({ loading: true });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&sortBy=popularity&apiKey=1ce45add0b8743919ba3f86a5b9db555&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&sortBy=popularity&apiKey=1ce45add0b8743919ba3f86a5b9db555&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
 
     this.setState({
       articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false
-    });
-  }
-
-  handlePrevClick = async () => {
-    this.setState({ loading: true });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&sortBy=popularity&apiKey=1ce45add0b8743919ba3f86a5b9db555&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    let data = await fetch(url);
-    let parsedData = await data.json();
-
-    this.setState({
-      page: this.state.page - 1,
-      articles: parsedData.articles,
-      loading: false
+      totalResults: parsedData.totalResults
     })
   }
 
-  handleNextClick = async () => {
-    this.setState({ loading: true });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&sortBy=popularity&apiKey=1ce45add0b8743919ba3f86a5b9db555&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+  async componentDidMount() {
+    this.updateNews();
+  }
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://gnews.io/api/v4/top-headlines?token=1f1585a2f072790768145142e2a82f99&lang=en&country=in`;
     let data = await fetch(url);
     let parsedData = await data.json();
 
     this.setState({
-      page: this.state.page + 1,
-      articles: parsedData.articles,
-      loading: false
-    });
-  }
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    })
+  };
 
   render() {
-    document.title = "Florange - Get your daily dose of news for free";
     return (
-      <div className="container">
-        <h2 className="my-4 text-center"><strong>Top headlines - {this.props.category}</strong></h2>
-        {this.state.loading && <Spinner />}
-        <div className='row'>
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <div className='col-md-3 mb-5' key={element.url}>
-              <NewsItems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : 'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg'} newsUrl={element.url} />
+      <>
+        <h2 className="my-4 text-center"><strong>Top headlines - {this.capitalizeFirst(this.props.category)}</strong></h2>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className='container'>
+            <div className='row'>
+              {this.state.articles.map((element) => {
+                return <div className='col-md-3 mb-5' key={element.url}>
+                  <NewsItems title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : 'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg'} newsUrl={element.url} author={element.author} date={element.publishedAt} />
+                </div>
+              })}
             </div>
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-primary mx-4 my-4" onClick={this.handlePrevClick}> &larr; Previous</button>
-          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 12)} type="button" className="btn btn-primary mx-4 my-4" onClick={this.handleNextClick}>Next &rarr; </button>
-
-        </div>
-      </div>
+          </div>
+        </InfiniteScroll>
+      </>
     )
   }
 }
--- class-based-comp-app/src/components/NewsItems.js
+++ class-based-comp-app/src/components/NewsItems.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date } = this.props;
     return (
       <div className="container">
-        <div className="card bg-dark" style={{ width: "18rem" }}>
+        <div className="card bg-dark">
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body bg-dark text-light">
-            <h5 className="card-title">{title.slice(0, 45) === title ? title:`${title.slice(0, 45)}...`}</h5>
-            <p className="card-text">{description.slice(0, 100) === description ? description:`${description.slice(0, 90)}...`}</p>
+            <h5 className="card-title">{title.slice(0, 45) === title ? title : `${title.slice(0, 45)}...`}</h5>
+            <p className="card-text">{description.slice(0, 100) === description ? description : `${description.slice(0, 90)}...`}</p>
+            <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {`${new Date(date).getDate()}-${new Date(date).getMonth()}-${new Date(date).getFullYear()}`}</small></p>
             <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read more</a>
           </div>
         </div>
