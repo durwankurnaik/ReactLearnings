4ebb4628e6985790cef506318ed14004513da350
Durwankur Naik <ndurwankur14@gmail.com>
2021-11-28 02:20:45 +0530

    Finished with authentication, fetch and create notes APIs.
    Finished making authentication system for our website. Also did 2 APIs for fetching all the notes if the user is logged in and to create new notes if the user is logged in, otherwise he have to login with valid credentials.

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
+
+const JWT_SECRET = 'DurwankurIsDoingGreatInCoding';
 
 
-// Create a User using: POST: "/api/auth/createuser". No login required
+// ROUTE 1: Create a User using: POST: "/api/auth/createuser". No login required
 router.post('/createuser',
     // From here, the validations are carried out
     [
@@ -16,7 +21,6 @@ router.post('/createuser',
     async (req, res) => {
         // If there are errors, return a bad request and the errors
         const errors = validationResult(req);
-
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
@@ -29,21 +33,81 @@ router.post('/createuser',
                 return res.status(400).json({ error: "This email address is already taken" });
             }
 
+            const salt = await bcrypt.genSalt(10);
+            const secPasswd = await bcrypt.hash(req.body.password, salt);
+
             // If there are no errors, then the below block will be automatically executed. Hence creating the user which will pass all the required validation checks.
             user = await User.create({
                 name: req.body.name,
                 age: req.body.age,
                 email: req.body.email,
-                password: req.body.password,
+                password: secPasswd,
             });
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
 
-            res.json(user); // Returns the user info to the Thunder client screen
+            res.json({ authToken }); // Returns the user info to the Thunder client screen
         } catch (error) {
             console.error(error);
             req.status(500).send("Some error occurred");
         }
         //   .then(user => res.json(user))
         //   .catch(err => console.log(err));
-    })
+    });
+
+// ROUTE 2: Authenticating a User using: POST: "/api/auth/login". No login required
+router.post('/login',
+    // Validation takes place here. 
+    [
+        body('email', 'Enter a valid Email').isEmail(),
+        body('password', 'Password cannot be blank').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ error: 'Please check your email and password again.' });
+            }
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if (!passwordCompare) {
+                return res.status(400).json({ error: 'Please check your email and password again.' });
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+            res.json({ authToken });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal server error.");
+        }
+    });
+
+// ROUTE 3: Get user details who are logged in using: POST "/api/auth/profile" . Login required
+router.post('/profile', fetchuser,
+    async (req, res) => {
+        try {
+            const userId = req.user.id;
+            const user = await User.findById(userId).select("-password");
+            res.send(user);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send("Internal server error.");
+        }
+    });
 
 module.exports = router;
--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
+var fetchuser = require('../middleware/fetchuser');
 
-router.get('/', (req, res) => {
-    res.json([]);
+
+// ROUTE 1: Get all the notes of the logged in user using GET: "/api/notes" Login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error.");
+    }
+
+})
+
+// ROUTE 2: Add a new note using POST: "/api/addnotes" Login required
+router.post('/addnotes', fetchuser, [
+    body('title', 'Title must be atleast 3 characters long').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters long').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        });
+        const savedNote = await note.save();
+
+        res.json(savedNote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal server error.");
+    }
 })
 
 module.exports = router;
