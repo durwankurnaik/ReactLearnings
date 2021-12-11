c494970763c5c168f5f08e83bac2d54d3a49f611
Durwankur Naik <ndurwankur14@gmail.com>
2021-12-11 12:32:33 +0530

    A test project to learn Redux and its use cases
    Here I don't really care about the UI of the site and only focus on the working of React Redux. I will be doing implementation of Redux and do some operations on it.

---
+++ reduxapp/src/components/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className="container">
+            <h2>Deposit/Withdraw money</h2>
+            <button className="btn btn-primary mx-2">-</button><strong>Update Balance</strong>
+            <button className="btn btn-primary mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
